74d37ca09078b9f0338482e26b7ad073a1774636
Belmar <ibelmara@cuprum.cl>
2018-11-03 14:17:54 -0300

    Se carga, plantilla de administracion

--- abuelo/views.py
+++ abuelo/views.py
@@ -11,3 +11,6 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request,'index.html')
+
+def abuelos(request):
+    return render(request, 'abuelos.html')
