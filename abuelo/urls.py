e26906470dbf488514145121cf93b42a8aa56156
franckiito <f.alvaradou@alumnos.duoc.cl>
2018-11-03 16:31:27 -0300

    Registrar Cuidador

--- abuelo/urls.py
+++ abuelo/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index, name = "index"),
-
+    path('cuidador/registrar', views.registrar, name="registrar"),
     path('abuelos',views.abuelos, name = "abuelos"),
     path('abuelos/crear', views.crear_abuelo, name="crear_abuelo")
 ]
--- abuelo/views.py
+++ abuelo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
+from .models import Cuidador 
 
 #sistema de autenticación 
 from django.contrib.auth import authenticate,logout, login as auth_login
@@ -11,6 +12,23 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request,'index.html')
+
+def registrar(request):
+    run = request.POST.get('run','')
+    nombre = request.POST.get('nombre','')
+    fechaNacimiento = request.POST.get('fechaNacimiento','')
+    correo = request.POST.get('correo','')
+    telefono = request.POST.get('telefono','')
+    direccion = request.POST.get('direccion','')
+    contrasenia = request.POST.get('contrasenia','')
+
+    cuidador = Cuidador.objects.filter(run=run)
+    if cuidador is None:
+        cuidador = Cuidador(run=run, nombre=nombre, fechaNacimiento=fechaNacimiento, correo=correo, telefono=telefono,  direccion=direccion, contrasenia= contrasenia)
+        cuidador.save()
+        return render(request,'index.html',{'mensaje':'El usuario fue registrado correctamente.'})
+    else:
+        return render(request,'index.html',{'mensaje':'El usuario ingresado ya esta registrado.'})
 
 def abuelos(request):
     return render(request, 'abuelos.html')
